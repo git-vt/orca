c6406b1d195ba0c16b4c3c0f61f2ae7872f07ba7
Joshua A. Bockenek <jabocken@vt.edu>
2017-02-11 00:04:14 +0000

    Moved VCG tactics work to VCG_Tests.thy
    git-svn-id: orca@125

--- WP1-Programming-Languages-Semantics/WP11-C-semantics/src/IMP-Lenses/Hoare.thy
+++ WP1-Programming-Languages-Semantics/WP11-C-semantics/src/IMP-Lenses/Hoare.thy
@@ -885,35 +885,4 @@ lemma
   apply hoare_solver
   oops
 
-subsection \<open>Tactics for Theorem Proving\<close>
-text \<open>The tactics below are used to prove the validity of complex Hoare triples/expressions
-semi-automatically.\<close>
-
-lemmas subst_exp_thms = Const.abs_eq bop.abs_eq drop_bexp.abs_eq subst.abs_eq uop.abs_eq
-(* subst_upd_var_def lens_indep_def *)
-
-ML \<open>
-fun vcg_match_tac ctxt = (ALLGOALS o REPEAT_ALL_NEW) (CHANGED o TRY o FIRST'
-                         [match_tac ctxt @{thms vcg}]);
-fun vcg_assms_insert_tac ctxt = (ALLGOALS) (FIRST'
-                                [Method.insert_tac ctxt (Assumption.all_prems_of ctxt)]);
-fun vcg_simp_tac ctxt = asm_full_simp_tac ctxt 1;
-fun vcg_simp_loop_tac ctxt = (ALLGOALS) (REPEAT o CHANGED o TRY o FIRST'
-                              [asm_full_simp_tac ctxt]);
-fun vcg_subst_exp_tac ctxt = (ALLGOALS o REPEAT_ALL_NEW) (CHANGED o TRY o FIRST'
-                             [EqSubst.eqsubst_tac ctxt [0] @{thms subst_exp_thms}]);
-
-val vcg_tac = vcg_match_tac THEN'
-              vcg_assms_insert_tac THEN'
-              vcg_simp_tac THEN'
-              vcg_subst_exp_tac THEN'
-              vcg_simp_loop_tac;
-
-val vcg_tacx = vcg_match_tac THEN'
-              vcg_assms_insert_tac THEN'
-              vcg_simp_tac THEN'
-              vcg_subst_exp_tac THEN'
-              vcg_simp_tac;
-\<close>
-
 end
