7e1a9342d3157a9c411089b7e7c99de7e44f1d00
Yakoub Nemouchi <YakoubN@users.noreply.github.com>
2017-02-14 23:50:12 +0000

    VCG_test
    git-svn-id: orca@143

--- WP1-Programming-Languages-Semantics/WP11-C-semantics/src/IMP-Lenses/VCG_Tests.thy
+++ WP1-Programming-Languages-Semantics/WP11-C-semantics/src/IMP-Lenses/VCG_Tests.thy
@@ -33,7 +33,7 @@ val vcg_tacx = vcg_match_tac THEN'
               vcg_simp_tac THEN'
               vcg_subst_exp_tac THEN'
               vcg_simp_tac;
-\<close>
+\<close> 
 
 lemma
   assumes "weak_lens X"
@@ -50,9 +50,11 @@ lemma if_true:
 lemma if_false:
   assumes "weak_lens X"
   shows "\<lbrace>(VAR X) =\<^sub>e \<guillemotleft>0::int\<guillemotright>\<rbrace>
-         IF TRUE THEN SKIP ELSE (X :== ((VAR X) +\<^sub>e \<guillemotleft>1\<guillemotright>))
-         \<lbrace>(VAR X) =\<^sub>e \<guillemotleft>0\<guillemotright>\<rbrace>"
-  by (tactic \<open>vcg_tac @{context}\<close>)
+         IF FALSE THEN SKIP ELSE (X :== ((VAR X) +\<^sub>e \<guillemotleft>1::int\<guillemotright>))
+         \<lbrace>(VAR X) =\<^sub>e \<guillemotleft>1::int\<guillemotright>\<rbrace>"
+  using assms unfolding subst_upd_var_def
+  apply transfer apply auto
+  by (tactic \<open>vcg_tacx @{context}\<close>)
 
 lemma swap_test:
   assumes "weak_lens X" and "weak_lens Y" and "weak_lens T"
@@ -65,6 +67,20 @@ lemma swap_test:
   apply (tactic \<open>vcg_tac @{context}\<close>)
 oops (* The sequential rule needs intermediate condition specifications for the generated schematic
 variables; seL4 examples do this manually, as shown in
-seL4/verification/l4v/tools/c-parser/testfiles/breakcontinue.thy *)
+seL4/verification/l4v/tools/c-parser/testfiles/breakcontinue.thy 
+
+*)
+
+lemma swap_test:
+  assumes "weak_lens X" and "weak_lens Y" and "weak_lens T"
+  and "X \<bowtie> Y" and "X \<bowtie> T" and "Y \<bowtie> T"
+  shows "\<lbrace>(VAR X) =\<^sub>e \<guillemotleft>0::int\<guillemotright> \<and>\<^sub>e (VAR Y) =\<^sub>e \<guillemotleft>1::int\<guillemotright>\<rbrace>
+         T :== (VAR X);
+         X :== (VAR Y);
+         Y :== (VAR T)
+         \<lbrace>(VAR X) =\<^sub>e \<guillemotleft>1::int\<guillemotright> \<and>\<^sub>e (VAR Y) =\<^sub>e \<guillemotleft>0::int\<guillemotright>\<rbrace>"
+  using assms unfolding lens_indep_def subst_upd_var_def
+  by hoare_solver
+  
 
 end
