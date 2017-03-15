d1eaf48a0ea1d4c2cb132a9e7ff78ef81314bd60
Yakoub Nemouchi <YakoubN@users.noreply.github.com>
2017-03-15 14:57:05 +0000

    fixing conflict
    git-svn-id: orca@179

--- WP1-Programming-Languages-Semantics/WP11-C-semantics/src/IMP-Lenses/utp/utp_hoare.thy
+++ WP1-Programming-Languages-Semantics/WP11-C-semantics/src/IMP-Lenses/utp/utp_hoare.thy
@@ -18,33 +18,38 @@ definition hoare_r :: "'\<alpha> cond \<Rightarrow> '\<alpha> hrel \<Rightarrow>
 
 declare hoare_r_def [upred_defs]
 
+lemma hoare_true [hoare]: "\<lbrace>p\<rbrace>C\<lbrace>true\<rbrace>\<^sub>u"
+  by rel_auto
+
+lemma hoare_false [hoare]: "\<lbrace>false\<rbrace>C\<lbrace>q\<rbrace>\<^sub>u"
+  by rel_auto
+
 subsection {*Hoare for Consequence*}
 
 lemma hoare_r_conseq [hoare]: 
-  assumes "`p\<^sub>1 \<Rightarrow> p\<^sub>2`" and "\<lbrace>p\<^sub>2\<rbrace>S\<lbrace>q\<^sub>2\<rbrace>\<^sub>u" and "`q\<^sub>2 \<Rightarrow> q\<^sub>1`" 
-  shows   "\<lbrace>p\<^sub>1\<rbrace>S\<lbrace>q\<^sub>1\<rbrace>\<^sub>u"
+  assumes "`p\<^sub>1 \<Rightarrow> p\<^sub>2`" and "\<lbrace>p\<^sub>2\<rbrace>C\<lbrace>q\<^sub>2\<rbrace>\<^sub>u" and "`q\<^sub>2 \<Rightarrow> q\<^sub>1`" 
+  shows   "\<lbrace>p\<^sub>1\<rbrace>C\<lbrace>q\<^sub>1\<rbrace>\<^sub>u"
   by (insert assms) rel_auto
 
 subsection {*Precondition strengthening*}
 
 lemma hoare_pre_str[hoare]:
-  assumes "`p\<^sub>1 \<Rightarrow> p\<^sub>2`" and "\<lbrace>p\<^sub>2\<rbrace>C\<lbrace>Q\<rbrace>\<^sub>u"
-  shows "\<lbrace>p\<^sub>1\<rbrace>C\<lbrace>Q\<rbrace>\<^sub>u" 
+  assumes "`p\<^sub>1 \<Rightarrow> p\<^sub>2`" and "\<lbrace>p\<^sub>2\<rbrace>C\<lbrace>q\<rbrace>\<^sub>u"
+  shows "\<lbrace>p\<^sub>1\<rbrace>C\<lbrace>q\<rbrace>\<^sub>u" 
   by (insert assms) rel_auto
 
 subsection {*Post-condition weakening*}
 
 lemma hoare_post_weak[hoare]:
-  assumes 1:"\<lbrace>P\<rbrace>C\<lbrace>Q\<^sub>2\<rbrace>\<^sub>u"  
-  and     2:"`Q\<^sub>2 \<Rightarrow> Q\<^sub>1`"
-  shows "\<lbrace>P\<rbrace>C\<lbrace>Q\<^sub>1\<rbrace>\<^sub>u" 
+  assumes "\<lbrace>p\<rbrace>C\<lbrace>q\<^sub>2\<rbrace>\<^sub>u" and "`q\<^sub>2 \<Rightarrow> q\<^sub>1`"
+  shows "\<lbrace>p\<rbrace>C\<lbrace>q\<^sub>1\<rbrace>\<^sub>u" 
  by (insert assms) rel_auto
 
 subsection {*Hoare and assertion logic*}
 
 lemma hoare_r_conj [hoare]: 
-  assumes"\<lbrace>p\<rbrace>Q\<lbrace>r\<rbrace>\<^sub>u" and "\<lbrace>p\<rbrace>Q\<lbrace>s\<rbrace>\<^sub>u"  
-  shows "\<lbrace>p\<rbrace>Q\<lbrace>r \<and> s\<rbrace>\<^sub>u"
+  assumes"\<lbrace>p\<rbrace>C\<lbrace>r\<rbrace>\<^sub>u" and "\<lbrace>p\<rbrace>C\<lbrace>s\<rbrace>\<^sub>u"  
+  shows "\<lbrace>p\<rbrace>C\<lbrace>r \<and> s\<rbrace>\<^sub>u"
   by (insert assms) rel_auto
 
 subsection {*Hoare SKIP*}
@@ -65,22 +70,22 @@ lemma assigns_hoare_r' [hoare]: "\<lbrace>\<sigma> \<dagger> p\<rbrace>\<langle>
 subsection {*Hoare for Sequential Composition*}
 
 lemma seq_hoare_r [hoare]: 
-  assumes"\<lbrace>p\<rbrace>Q\<^sub>1\<lbrace>s\<rbrace>\<^sub>u" and "\<lbrace>s\<rbrace>Q\<^sub>2\<lbrace>r\<rbrace>\<^sub>u" 
-  shows"\<lbrace>p\<rbrace>Q\<^sub>1 ;; Q\<^sub>2\<lbrace>r\<rbrace>\<^sub>u"
+  assumes"\<lbrace>p\<rbrace>C\<^sub>1\<lbrace>s\<rbrace>\<^sub>u" and "\<lbrace>s\<rbrace>C\<^sub>2\<lbrace>r\<rbrace>\<^sub>u" 
+  shows"\<lbrace>p\<rbrace>C\<^sub>1 ;; C\<^sub>2\<lbrace>r\<rbrace>\<^sub>u"
   by (insert assms) rel_auto
 
 subsection {*Hoare for Conditional*}
 
 lemma cond_hoare_r [hoare]: 
-  assumes "\<lbrace>b \<and> p\<rbrace>S\<lbrace>q\<rbrace>\<^sub>u" and "\<lbrace>\<not>b \<and> p\<rbrace>T\<lbrace>q\<rbrace>\<^sub>u"
-  shows "\<lbrace>p\<rbrace>S \<triangleleft> b \<triangleright>\<^sub>r T\<lbrace>q\<rbrace>\<^sub>u"
+  assumes "\<lbrace>b \<and> p\<rbrace>C\<^sub>1\<lbrace>q\<rbrace>\<^sub>u" and "\<lbrace>\<not>b \<and> p\<rbrace>C\<^sub>2\<lbrace>q\<rbrace>\<^sub>u" 
+  shows "\<lbrace>p\<rbrace>C\<^sub>1 \<triangleleft> b \<triangleright>\<^sub>r C\<^sub>2\<lbrace>q\<rbrace>\<^sub>u"
   by (insert assms) rel_auto
 
 subsection {*Hoare for While-loop*}
 
 lemma while_hoare_r [hoare]:
-  assumes "\<lbrace>p \<and> b\<rbrace>S\<lbrace>p\<rbrace>\<^sub>u"
-  shows "\<lbrace>p\<rbrace>WHILE b DO S OD\<lbrace>\<not>b \<and> p\<rbrace>\<^sub>u"
+  assumes "\<lbrace>p \<and> b\<rbrace>C\<lbrace>p\<rbrace>\<^sub>u"
+  shows "\<lbrace>p\<rbrace>WHILE b DO C OD\<lbrace>\<not>b \<and> p\<rbrace>\<^sub>u"
   using assms
   by (simp add: While_def hoare_r_def, rule_tac lfp_lowerbound) (rel_auto)
 
@@ -91,16 +96,9 @@ lemma while_hoare_r' [hoare]:
   by (metis conj_comm hoare_r_conseq p_imp_p taut_true while_hoare_r)
 
 lemma while_invr_hoare_r [hoare]:
-  assumes "\<lbrace>p \<and> b\<rbrace>S\<lbrace>p\<rbrace>\<^sub>u" "`pre \<Rightarrow> p`" "`(\<not>b \<and> p) \<Rightarrow> post`"
-  shows "\<lbrace>pre\<rbrace>while b invr p do S od\<lbrace>post\<rbrace>\<^sub>u"
+  assumes "\<lbrace>p \<and> b\<rbrace>C\<lbrace>p\<rbrace>\<^sub>u" and "`pre \<Rightarrow> p`" and "`(\<not>b \<and> p) \<Rightarrow> post`"
+  shows "\<lbrace>pre\<rbrace>while b invr p do C od\<lbrace>post\<rbrace>\<^sub>u"
   by (metis assms hoare_r_conseq while_hoare_r while_inv_def)
 
-subsection Miscellaneous
-
-lemma hoare_r_post_true [hoare]: "\<lbrace>p\<rbrace>S\<lbrace>true\<rbrace>\<^sub>u"
-  by rel_auto
-
-lemma hoare_r_pre_false [hoare]: "\<lbrace>false\<rbrace>S\<lbrace>q\<rbrace>\<^sub>u"
-  by rel_auto
 
 end
