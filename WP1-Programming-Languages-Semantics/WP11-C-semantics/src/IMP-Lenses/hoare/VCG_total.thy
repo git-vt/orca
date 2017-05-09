ba70e26ce4c7d2ba196455a6d293ff93536c6cfe
Joshua A. Bockenek <jabocken@vt.edu>
2017-05-09 01:47:45 +0000

    Added assumptions
    git-svn-id: orca@219

--- WP1-Programming-Languages-Semantics/WP11-C-semantics/src/IMP-Lenses/hoare/VCG_total.thy
+++ WP1-Programming-Languages-Semantics/WP11-C-semantics/src/IMP-Lenses/hoare/VCG_total.thy
@@ -4,6 +4,18 @@ theory VCG_total
   imports utp_hoare_total
 begin
 
+method rules =
+  rule seq_hoare_r_t|
+  rule skip_hoare_r_t|
+  rule cond_hoare_r_t|
+  rule assigns_hoare_r'_t| (* merges two subgoals? *)
+  rule assigns_hoare_r_t|  (* may not be useful *)
+  rule assert_hoare_r_t|
+  rule assume_hoare_r_t|
+  rule while_hoare_r_t|
+  rule while_hoare_r'_t|
+  rule while_invr_hoare_r_t
+
 value "[x. x \<in> {0::int, 1, 2}]"
 (* alphabet 'a josh = xs :: \<open>'a list\<close> *)
 
@@ -15,11 +27,13 @@ lemma bubble_sort_manual:
   shows
   "\<lbrace>true\<rbrace>
   n \<Midarrow> #\<^sub>u(&xs);;
+  (#\<^sub>u(&xs) \<ge>\<^sub>u &n \<and> &n \<ge>\<^sub>u 0)\<^sup>\<top>\<^sup>C;;
   while &n >\<^sub>u \<guillemotleft>0::nat\<guillemotright>
   invr #\<^sub>u(&xs) \<ge>\<^sub>u &n \<and> &n \<ge>\<^sub>u 0
   do
     newn \<Midarrow> 0;;
     i \<Midarrow> \<guillemotleft>1::nat\<guillemotright>;;
+    (&n-1 \<ge>\<^sub>u &i \<and> &i \<ge>\<^sub>u 1)\<^sup>\<top>\<^sup>C;;
     while &i \<le>\<^sub>u &n-1
     invr &n-1 \<ge>\<^sub>u &i \<and> &i \<ge>\<^sub>u 1
     do
