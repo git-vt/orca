2eb9bd3d0aeb1777e2a5a34eae44cee7c63b467f
Joshua A. Bockenek <jabocken@vt.edu>
2017-05-15 16:04:11 +0000

    Showing that infixl is no better than infixr for seq
    git-svn-id: orca@230

--- WP1-Programming-Languages-Semantics/WP11-C-semantics/src/IMP-Lenses/examples/examples_infixl_seq.thy
+++ WP1-Programming-Languages-Semantics/WP11-C-semantics/src/IMP-Lenses/examples/examples_infixl_seq.thy
@@ -10,7 +10,7 @@ notation useq (infixl ";;" 51)
 
 text \<open>Variable swapping\<close>
 
-lemma swap_test_manual1:
+lemma swap_test1:
   assumes "weak_lens x" and "weak_lens y" and "weak_lens z"
   and "x \<bowtie> y" and "x \<bowtie> z" and "y \<bowtie> z"
   shows "\<lbrace>&x =\<^sub>u \<guillemotleft>a\<guillemotright> \<and> &y =\<^sub>u \<guillemotleft>b\<guillemotright>\<rbrace>
@@ -29,7 +29,7 @@ lemma swap_test_manual1:
   apply (simp add: lens_indep_sym)
   done
 
-lemma swap_test_manual2:
+lemma swap_test2:
   assumes "weak_lens x" and "weak_lens y" and "weak_lens z"
   and "x \<bowtie> y" and "x \<bowtie> z" and "y \<bowtie> z"
   shows "\<lbrace>&x =\<^sub>u \<guillemotleft>a\<guillemotright> \<and> &y =\<^sub>u \<guillemotleft>b\<guillemotright>\<rbrace>
@@ -48,5 +48,62 @@ lemma swap_test_manual2:
   apply pred_simp
   apply (simp add: lens_indep_sym)
   done
+
+subsection \<open>Adding in loops\<close>
+
+lemma increment:
+  assumes "vwb_lens x" and "x \<bowtie> y"
+  shows
+  "\<lbrace>&y =\<^sub>u \<guillemotleft>5::int\<guillemotright>\<rbrace>
+  x \<Midarrow> 0;;
+  (&x =\<^sub>u 0 \<and> &y =\<^sub>u 5)\<^sup>\<top>\<^sup>C;;
+  while &x <\<^sub>u &y
+  invr &x \<le>\<^sub>u &y \<and> &y =\<^sub>u 5
+  do x \<Midarrow> &x + 1 od
+  \<lbrace>&x =\<^sub>u 5\<rbrace>\<^sub>D"
+  apply (insert assms)
+  apply (rule seq_hoare_r_t)
+   apply (rule seq_hoare_r_t[of _ _ true])
+    apply (rule assigns_hoare_r_t)
+    apply pred_auto
+   apply (rule assume_hoare_r_t)
+    apply (rule skip_hoare_r_t)
+   apply rel_auto
+  apply (rule while_invr_hoare_r_t)
+    apply (rule assigns_hoare_r_t)
+    unfolding lens_indep_def
+    apply pred_auto
+   apply pred_auto
+  apply pred_auto
+  done
+
+lemma increment2:
+  assumes "vwb_lens x" and "x \<bowtie> y"
+  shows
+  "\<lbrace>&y =\<^sub>u \<guillemotleft>5::int\<guillemotright>\<rbrace>
+  x \<Midarrow> 0;;
+  (&x =\<^sub>u 0 \<and> &y =\<^sub>u 5)\<^sup>\<top>\<^sup>C;;
+  while &x <\<^sub>u &y
+  invr &x \<le>\<^sub>u &y \<and> &y =\<^sub>u 5
+  do x \<Midarrow> &x + 1 od
+  \<lbrace>&x =\<^sub>u 5\<rbrace>\<^sub>D"
+  apply (insert assms)
+  apply (rule seq_hoare_r_t)
+   defer
+   apply (rule while_invr_hoare_r_t)
+     apply (rule assigns_hoare_r_t)
+     unfolding lens_indep_def
+     apply pred_auto
+    defer
+    apply pred_auto
+   apply (rule seq_hoare_r_t)
+    defer
+    apply (rule assume_hoare_r_t)
+     apply (rule skip_hoare_r_t)
+    apply rel_auto
+   apply pred_auto
+  apply (rule assigns_hoare_r_t)
+  apply pred_auto
+  oops (*  (rule seq_hoare_r_t[of _ _ true]) is necessary to instantiate ?s27 *)
 
 end
