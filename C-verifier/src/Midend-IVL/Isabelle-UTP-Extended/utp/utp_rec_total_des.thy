8d45f2e293ca37c80f74002c5e87d9af78c0a7c4
Yakoub Nemouchi <YakoubN@users.noreply.github.com>
2017-10-04 05:50:23 +0000

    Total correctness recursion rule for designs
    git-svn-id: orca@456

--- C-verifier/src/Midend-IVL/Isabelle-UTP-Extended/utp/utp_rec_total_des.thy
+++ C-verifier/src/Midend-IVL/Isabelle-UTP-Extended/utp/utp_rec_total_des.thy
@@ -41,30 +41,28 @@ lemma  rec_total_rule_des:
    apply assumption
     apply assumption
   done
-    
-lemma 1:  
-   assumes   WF: "wf R"
-  assumes "$ok\<acute> \<sharp> Pre" "$ok\<acute> \<sharp> Post"
-  assumes "(\<And>st'. (st', st) \<in> R \<Longrightarrow> ((Pre \<and> \<lceil>E\<rceil>\<^sub>< =\<^sub>u \<guillemotleft>st'\<guillemotright> ) \<turnstile> Post) \<sqsubseteq> \<mu>\<^sub>D B)"
-  shows "(( Pre \<and> (\<lceil>E\<rceil>\<^sub><,\<guillemotleft>st\<guillemotright>)\<^sub>u\<in>\<^sub>u\<guillemotleft>R\<guillemotright>) \<turnstile>  Post) \<sqsubseteq> \<mu>\<^sub>D B"
+ thm monoD   
+lemma Mono_thy_orderD:
+  assumes M: "Mono\<^bsub>uthy_order DES\<^esub> B"
+  (*and     H: "B \<in> \<lbrakk>\<^bold>H\<rbrakk>\<^sub>H \<rightarrow> \<lbrakk>\<^bold>H\<rbrakk>\<^sub>H" what is the relation of H with other assumptions*)
+  and       "x is \<H>\<^bsub>DES\<^esub>"
+   and       "y is \<H>\<^bsub>DES\<^esub>"
+  and      "y \<sqsubseteq> x"    
+shows   "(B y) \<sqsubseteq> (B x)"
   using assms
-  by (pred_blast)
+  by (auto simp add: isotone_def utp_weak_partial_order)
+
     
-lemma 2:  
-   assumes   WF: "wf R"
-  assumes "$ok\<acute> \<sharp> Pre" "$ok\<acute> \<sharp> Post"
-  shows "\<And>st. (\<And>st'. (st'  , st) \<in> R \<Longrightarrow> ((Pre \<and> \<lceil>E\<rceil>\<^sub>< =\<^sub>u \<guillemotleft>st'\<guillemotright> ) \<turnstile> Post) \<sqsubseteq> \<mu>\<^sub>D B) \<Longrightarrow>
-         (( Pre \<and> (\<lceil>E\<rceil>\<^sub><,\<guillemotleft>st\<guillemotright>)\<^sub>u\<in>\<^sub>u\<guillemotleft>R\<guillemotright>) \<turnstile>  Post) \<sqsubseteq> \<mu>\<^sub>D B"
-  by (pred_blast)
-    
-find_theorems "LFP" "mono"
+  thm Mono_utp_orderI
+      monoI monoD
 lemma  rec_total_utp_des_rule: 
   assumes   WF: "wf R"
     and      M: "Mono\<^bsub>uthy_order DES\<^esub> B"
     and      H: "B \<in> \<lbrakk>\<^bold>H\<rbrakk>\<^sub>H \<rightarrow> \<lbrakk>\<^bold>H\<rbrakk>\<^sub>H"
+    and      Okey1:"$ok\<acute> \<sharp> Pre" and Okey2:"$ok\<acute> \<sharp> Post"
     and  induct_step:
     "\<And>st. ((Pre \<and>\<lceil>E\<rceil>\<^sub>< =\<^sub>u \<guillemotleft>st\<guillemotright>) \<turnstile> Post) \<sqsubseteq> (B ((Pre \<and> (\<lceil>E\<rceil>\<^sub><,\<guillemotleft>st\<guillemotright>)\<^sub>u\<in>\<^sub>u\<guillemotleft>R\<guillemotright>) \<turnstile> Post))"
-  shows "(Pre \<turnstile> Post) \<sqsubseteq> \<mu>\<^sub>D B"  
+  shows "(Pre \<turnstile> Post) \<sqsubseteq> \<mu>\<^sub>D B"            
 proof -          
   { fix st
     have "((Pre \<and>\<lceil>E\<rceil>\<^sub>< =\<^sub>u \<guillemotleft>st\<guillemotright>) \<turnstile> Post)\<sqsubseteq> (\<mu>\<^sub>D B)" 
@@ -73,15 +71,27 @@ proof -
       apply (subst design_theory_continuous.LFP_unfold [OF M H])
     proof -
       fix st 
-      assume *: "\<And>st'. (st'  , st) \<in> R \<Longrightarrow> ((Pre \<and>\<lceil>E\<rceil>\<^sub>< =\<^sub>u \<guillemotleft>st'\<guillemotright>) \<turnstile> Post) \<sqsubseteq> \<mu>\<^sub>D B"
-      hence "((Pre \<and> (\<lceil>E\<rceil>\<^sub><,\<guillemotleft>st\<guillemotright>)\<^sub>u\<in>\<^sub>u\<guillemotleft>R\<guillemotright>) \<turnstile> Post) \<sqsubseteq> \<mu>\<^sub>D B"  
+      assume *: "\<And>st'. (st', st) \<in> R \<Longrightarrow> ((Pre \<and>\<lceil>E\<rceil>\<^sub>< =\<^sub>u \<guillemotleft>st'\<guillemotright>) \<turnstile> Post) \<sqsubseteq> \<mu>\<^sub>D B"
+      from * have 0:"((Pre \<and> (\<lceil>E\<rceil>\<^sub><,\<guillemotleft>st\<guillemotright>)\<^sub>u\<in>\<^sub>u\<guillemotleft>R\<guillemotright>) \<turnstile> Post) \<sqsubseteq> \<mu>\<^sub>D B"  
         by pred_blast
-      hence 1: "B ((Pre \<and> (\<lceil>E\<rceil>\<^sub><,\<guillemotleft>st\<guillemotright>)\<^sub>u\<in>\<^sub>u\<guillemotleft>R\<guillemotright> \<Rightarrow> Post)) \<sqsubseteq> B (\<mu>\<^sub>D B)"    
-        by (rule monoD[OF M])
-      have 2: "(Pre \<and> \<lceil>E\<rceil>\<^sub>< =\<^sub>u \<guillemotleft>st\<guillemotright> \<Rightarrow> Post) \<sqsubseteq> \<dots>" 
-        by (rule induct_step)  
-      show "(Pre \<and> \<lceil>E\<rceil>\<^sub>< =\<^sub>u \<guillemotleft>st\<guillemotright> \<Rightarrow> Post) \<sqsubseteq> B (\<mu> B) "  
-        using order_trans[OF 1 2] .
+     from M H design_theory_continuous.LFP_lemma3 have 1: "\<mu>\<^sub>D B \<sqsubseteq>  B (\<mu>\<^sub>D B)"
+       by auto     
+     from 0 1 have 2:"((Pre \<and> (\<lceil>E\<rceil>\<^sub><,\<guillemotleft>st\<guillemotright>)\<^sub>u\<in>\<^sub>u\<guillemotleft>R\<guillemotright>) \<turnstile> Post) \<sqsubseteq> B(\<mu>\<^sub>D B)"
+       by simp
+     have 3: "B((Pre \<and> (\<lceil>E\<rceil>\<^sub><,\<guillemotleft>st\<guillemotright>)\<^sub>u\<in>\<^sub>u\<guillemotleft>R\<guillemotright>)  \<turnstile> Post) \<sqsubseteq> B (\<mu>\<^sub>D B)"
+       thm Mono_thy_orderD
+        apply (rule Mono_thy_orderD[OF M])
+         apply (metis (no_types) Healthy_def' des_hcond_def design_theory_continuous.LFP_closed)
+         prefer 2
+       apply (simp add: "0")
+        unfolding prod_hcond_def Healthy_def utp_designs.des_hcond_def H1_def H2_def J_def
+          using Okey1 Okey2
+          apply rel_simp
+          apply fastforce 
+         done
+        have 4:"((Pre \<and> \<lceil>E\<rceil>\<^sub>< =\<^sub>u \<guillemotleft>st\<guillemotright>) \<turnstile> Post) \<sqsubseteq> \<dots>"  by (rule induct_step)
+      show "((Pre \<and> \<lceil>E\<rceil>\<^sub>< =\<^sub>u \<guillemotleft>st\<guillemotright>) \<turnstile> Post) \<sqsubseteq> B (\<mu>\<^sub>D B) "  
+        using order_trans[OF 3 4] .
     qed  
   }
   thus ?thesis
