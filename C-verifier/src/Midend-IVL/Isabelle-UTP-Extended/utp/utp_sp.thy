c539654b53bc522b215ba68f8fc9260159d13066
Yakoub Nemouchi <YakoubN@users.noreply.github.com>
2017-10-17 01:16:34 +0000

    git-svn-id: orca@497

--- C-verifier/src/Midend-IVL/Isabelle-UTP-Extended/utp/utp_sp.thy
+++ C-verifier/src/Midend-IVL/Isabelle-UTP-Extended/utp/utp_sp.thy
@@ -11,36 +11,32 @@ consts
   usp :: "'a \<Rightarrow> 'b \<Rightarrow> 'c" (infix "sp" 60)
   
 definition sp_upred :: "'\<alpha> cond \<Rightarrow> ('\<alpha>, '\<beta>) rel \<Rightarrow> '\<beta> cond" where
-"sp_upred p Q = \<lfloor> ((\<lceil>p\<rceil>\<^sub>>) ;; Q) :: ('\<alpha>, '\<beta>) rel\<rfloor>\<^sub>>"
+"sp_upred p Q = \<lfloor>(\<lceil>p\<rceil>\<^sub>> ;; Q) :: ('\<alpha>, '\<beta>) rel\<rfloor>\<^sub>>"
 
 adhoc_overloading
   usp sp_upred
   
-declare sp_upred_def [urel_defs]
+declare sp_upred_def [upred_defs]
 
-lemma sp_false [sp]: "q sp false  = false"
+lemma sp_false [sp]: "p sp false = false"
   by (rel_simp) 
     
 lemma sp_assigns_r [sp]: 
-  "vwb_lens x \<Longrightarrow> ( p sp x :== e ) = (\<^bold>\<exists>v \<bullet> p\<lbrakk>\<guillemotleft>v\<guillemotright>/x\<rbrakk> \<and> &x =\<^sub>u e\<lbrakk>\<guillemotleft>v\<guillemotright>/x\<rbrakk>)"
+  "vwb_lens x \<Longrightarrow> (p sp x :== e ) = (\<^bold>\<exists>v \<bullet> p\<lbrakk>\<guillemotleft>v\<guillemotright>/x\<rbrakk> \<and> &x =\<^sub>u e\<lbrakk>\<guillemotleft>v\<guillemotright>/x\<rbrakk>)"
   apply (rel_simp) 
     apply transfer
-
   apply auto
    apply (rule_tac x = \<open>get\<^bsub>x\<^esub> y\<close> in exI)
     apply simp
   apply (metis vwb_lens.put_eq)
-    done
-
- 
+  done
+    
 theorem sp_hoare_link:
-  "\<lbrace>p\<rbrace>Q\<lbrace>r\<rbrace>\<^sub>u \<longleftrightarrow> (r \<sqsubseteq> p sp Q  )"
+  "\<lbrace>p\<rbrace>Q\<lbrace>r\<rbrace>\<^sub>u \<longleftrightarrow> (r \<sqsubseteq> p sp Q)"
   by rel_auto   
 
 lemma "\<lbrace>p\<rbrace>C\<lbrace>p sp C\<rbrace>\<^sub>u"
-  apply pred_simp
-  apply rel_blast
-  done  
-
+  by rel_blast
+  
 end  
   
