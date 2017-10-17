fb8c1b565dbcd01fa51c8b191334a002db207afa
Yakoub Nemouchi <YakoubN@users.noreply.github.com>
2017-10-17 00:34:27 +0000

    git-svn-id: orca@495

--- C-verifier/src/Midend-IVL/Isabelle-UTP-Extended/utp/utp_rec_total.thy
+++ C-verifier/src/Midend-IVL/Isabelle-UTP-Extended/utp/utp_rec_total.thy
@@ -136,6 +136,59 @@ definition while_invT :: "'\<alpha> cond \<Rightarrow> '\<alpha> cond \<Rightarr
 
 lemma while_wf_hoare_r:
   assumes WF: "wf R"
+<<<<<<< .mine
+  assumes I0: "`Pre \<Rightarrow> I`"
+  assumes induct_step:"\<And> st. (\<lceil>b \<and> I \<and> E =\<^sub>u \<guillemotleft>st\<guillemotright>\<rceil>\<^sub>< \<Rightarrow> (\<lceil>I \<and>(E,\<guillemotleft>st\<guillemotright>)\<^sub>u\<in>\<^sub>u\<guillemotleft>R\<guillemotright>\<rceil>\<^sub>> )) \<sqsubseteq> f"  
+  assumes PHI:"`\<not> \<lceil>b\<rceil>\<^sub>< \<and> \<lceil>I\<rceil>\<^sub>< \<Rightarrow> \<lceil>Post\<rceil>\<^sub>>`"  
+  shows "\<lbrace>Pre\<rbrace>while\<^sub>\<bottom> b invr I do f od\<lbrace> Post\<rbrace>\<^sub>u"
+  unfolding  hoare_r_def while_invT_def while_bot_def
+   apply (rule pre_weak_rel[of _ "\<lceil>I\<rceil>\<^sub><" ])
+  using I0 
+   apply pred_simp
+    apply (rule rec_total_utp_rule[where Pre="\<lceil>I\<rceil>\<^sub><" and E = E, OF WF])  
+   apply (simp add: cond_mono monoI seqr_mono)
+  apply (rule  cond_refine_rel)
+     prefer 2
+   apply (rule skip_refine_rel)
+  using PHI
+   apply pred_blast
+    subgoal for  st 
+      apply (rule_tac seq_refine_unrest[where s= "I \<and> (E,\<guillemotleft>st\<guillemotright>)\<^sub>u\<in>\<^sub>u\<guillemotleft>R\<guillemotright>" ])
+         apply pred_simp
+        apply pred_simp
+       apply (rule order_trans[OF induct_step, where st1 = st]) 
+       apply pred_simp 
+      apply pred_simp
+done        
+  done 
+
+||||||| .r487
+  assumes I0: "`Pre \<Rightarrow> I`"
+  assumes induct_step:"\<And> st. (\<lceil>b \<and> I \<and>  E =\<^sub>u \<guillemotleft>st\<guillemotright> \<rceil>\<^sub>< \<Rightarrow> (\<lceil>I \<and>(E,\<guillemotleft>st\<guillemotright>)\<^sub>u\<in>\<^sub>u\<guillemotleft>R\<guillemotright> \<rceil>\<^sub>> )) \<sqsubseteq> f"  
+  assumes PHI:"`\<not> \<lceil>b\<rceil>\<^sub>< \<and> \<lceil>I\<rceil>\<^sub>< \<Rightarrow> \<lceil>Post\<rceil>\<^sub>>`"  
+  shows "\<lbrace>Pre\<rbrace>while\<^sub>\<bottom> b invr I do f od\<lbrace> Post\<rbrace>\<^sub>u"
+  unfolding  hoare_r_def while_invT_def while_bot_def
+   apply (rule pre_weak_rel[of _ "\<lceil>I\<rceil>\<^sub><" ])
+  using I0 
+   apply pred_simp
+    apply (rule rec_total_utp_rule[where Pre="\<lceil>I\<rceil>\<^sub><" and E = E, OF WF])  
+   apply (simp add: cond_mono monoI seqr_mono)
+  apply (rule  cond_refine_rel)
+     prefer 2
+   apply (rule skip_refine_rel)
+  using PHI
+   apply pred_blast
+    subgoal for  st 
+      apply (rule_tac seq_refine_unrest[where s= "I \<and> (E,\<guillemotleft>st\<guillemotright>)\<^sub>u\<in>\<^sub>u\<guillemotleft>R\<guillemotright>" ])
+         apply pred_simp
+        apply pred_simp
+       apply (rule order_trans[OF induct_step, where st1 = st]) 
+       apply pred_simp 
+      apply pred_simp
+done        
+  done 
+
+=======
   assumes I0: "`pre \<Rightarrow> I`"
   assumes induct_step:"\<And> st. \<lbrace>b \<and> I \<and> e =\<^sub>u \<guillemotleft>st\<guillemotright>\<rbrace>Q\<lbrace>I \<and> (e, \<guillemotleft>st\<guillemotright>)\<^sub>u \<in>\<^sub>u \<guillemotleft>R\<guillemotright>\<rbrace>\<^sub>u"
   assumes PHI:"`(\<not>b \<and> I) \<Rightarrow> post`"  
@@ -156,4 +209,5 @@ proof (rule pre_weak_rel[of _ "\<lceil>I\<rceil>\<^sub><" ])
   qed       
 qed
  
+>>>>>>> .r493
 end
