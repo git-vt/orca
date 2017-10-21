c25227ff45cbce109084fc85b77d6d3b14fe3ab7
Yakoub Nemouchi <YakoubN@users.noreply.github.com>
2017-10-21 02:02:58 +0000

    Total correctness with hiding healthyness conditions
    git-svn-id: orca@500

--- C-verifier/src/Midend-IVL/Isabelle-UTP-Extended/hoare/HoareLogic/TotalCorrectness/Des/utp_hoare_des.thy
+++ C-verifier/src/Midend-IVL/Isabelle-UTP-Extended/hoare/HoareLogic/TotalCorrectness/Des/utp_hoare_des.thy
@@ -4,64 +4,64 @@ imports "../../../../utp/utp_rec_total_des"
 
 begin
 section {*Hoare logic for designs*}  
-named_theorems hoare_total
+named_theorems hoare_des
 
 subsection {*Hoare triple definition*}
 
 definition hoare_d :: "'\<alpha> cond \<Rightarrow> '\<alpha> hrel_des \<Rightarrow> '\<alpha> cond \<Rightarrow> bool" ("\<lbrace>_\<rbrace>_\<lbrace>_\<rbrace>\<^sub>D") where
 [upred_defs]:"\<lbrace>p\<rbrace>Q\<lbrace>r\<rbrace>\<^sub>D = ((p \<turnstile>\<^sub>n \<lceil>r\<rceil>\<^sub>>) \<sqsubseteq> Q)"
 
-lemma hoare_true_assisgns_d_t [hoare_total]: 
+lemma hoare_true_assisgns_d_t [hoare_des]: 
   "\<lbrace>p\<rbrace>\<langle>\<sigma>\<rangle>\<^sub>D\<lbrace>true\<rbrace>\<^sub>D"
   by rel_auto
 
-lemma hoare_true_skip_d_t [hoare_total]: 
+lemma hoare_true_skip_d_t [hoare_des]: 
   "\<lbrace>p\<rbrace>SKIP\<^sub>D\<lbrace>true\<rbrace>\<^sub>D"
   by rel_auto
 
-lemma hoare_false_d_t [hoare_total]: 
+lemma hoare_false_d_t [hoare_des]: 
   "\<lbrace>false\<rbrace>C\<lbrace>q\<rbrace>\<^sub>D"
   by rel_auto
 
 subsection {*Precondition weakening*}
 
-lemma hoare_pre_str_d_t[hoare_total]:
+lemma hoare_pre_str_d_t[hoare_des]:
   assumes "`p\<^sub>1 \<Rightarrow> p\<^sub>2`" and "\<lbrace>p\<^sub>2\<rbrace>C\<lbrace>q\<rbrace>\<^sub>D"
   shows "\<lbrace>p\<^sub>1\<rbrace>C\<lbrace>q\<rbrace>\<^sub>D" 
   by (insert assms) rel_auto
 
 subsection {*Post-condition strengthening*}
 
-lemma hoare_post_weak_d_t[hoare_total]:
+lemma hoare_post_weak_d_t[hoare_des]:
   assumes "\<lbrace>p\<rbrace>C\<lbrace>q\<^sub>2\<rbrace>\<^sub>D" and "`q\<^sub>2 \<Rightarrow> q\<^sub>1`"
   shows "\<lbrace>p\<rbrace>C\<lbrace>q\<^sub>1\<rbrace>\<^sub>D" 
  by (insert assms) rel_auto
 
 subsection {*Hoare and assertion logic*}
 
-lemma hoare_r_conj_d_t [hoare_total]: 
+lemma hoare_r_conj_d_t [hoare_des]: 
   assumes"\<lbrace>p\<rbrace>C\<lbrace>r\<rbrace>\<^sub>D" and "\<lbrace>p\<rbrace>C\<lbrace>s\<rbrace>\<^sub>D"
   shows "\<lbrace>p\<rbrace>C\<lbrace>r \<and> s\<rbrace>\<^sub>D"
   by (insert assms) rel_auto
 
 subsection {*Hoare SKIP*}
 
-lemma skip_d_hoare_d_t [hoare_total]: 
+lemma skip_d_hoare_d_t [hoare_des]: 
   "\<lbrace>p\<rbrace>SKIP\<^sub>D\<lbrace>p\<rbrace>\<^sub>D"
   by rel_auto
 
 subsection {*Hoare for assignment*}
 
-lemma assigns_d_hoare_d_t [hoare_total]: 
+lemma assigns_d_hoare_d_t [hoare_des]: 
   assumes"`p \<Rightarrow> \<sigma> \<dagger> q`" 
   shows  "\<lbrace>p\<rbrace>\<langle>\<sigma>\<rangle>\<^sub>D\<lbrace>q\<rbrace>\<^sub>D"
   by (insert assms) rel_auto
 
-lemma assigns_d_hoare_d'_t [hoare_total]: 
+lemma assigns_d_hoare_d'_t [hoare_des]: 
   "\<lbrace>\<sigma> \<dagger> p\<rbrace>\<langle>\<sigma>\<rangle>\<^sub>D\<lbrace>p\<rbrace>\<^sub>D"
   by rel_auto
 
-lemma assigns_d_floyd_d_t [hoare_total]:
+lemma assigns_d_floyd_d_t [hoare_des]:
   assumes \<open>vwb_lens x\<close>
   shows \<open>\<lbrace>p\<rbrace>x :=\<^sub>D e\<lbrace>\<^bold>\<exists>v \<bullet> p\<lbrakk>\<guillemotleft>v\<guillemotright>/x\<rbrakk> \<and> &x =\<^sub>u e\<lbrakk>\<guillemotleft>v\<guillemotright>/x\<rbrakk>\<rbrace>\<^sub>D\<close>
   apply (insert assms)
@@ -73,19 +73,19 @@ lemma assigns_d_floyd_d_t [hoare_total]:
 
 subsection {*Hoare for Sequential Composition*}
 
-lemma seq_hoare_d_t [hoare_total]: 
+lemma seq_hoare_d_t [hoare_des]: 
   assumes"\<lbrace>p\<rbrace>C\<^sub>1\<lbrace>s\<rbrace>\<^sub>D" and "\<lbrace>s\<rbrace>C\<^sub>2\<lbrace>r\<rbrace>\<^sub>D" 
   shows"\<lbrace>p\<rbrace>C\<^sub>1 ;; C\<^sub>2\<lbrace>r\<rbrace>\<^sub>D"
   by (insert assms, rel_auto) metis+ 
 
 subsection {*Hoare for Conditional*}
 
-lemma cond_d_hoare_d_t [hoare_total]: 
+lemma cond_d_hoare_d_t [hoare_des]: 
   assumes "\<lbrace>b \<and> p\<rbrace>C\<^sub>1\<lbrace>q\<rbrace>\<^sub>D" and "\<lbrace>\<not>b \<and> p\<rbrace>C\<^sub>2\<lbrace>q\<rbrace>\<^sub>D" 
   shows "\<lbrace>p\<rbrace>C\<^sub>1 \<triangleleft> \<lceil>b\<rceil>\<^sub>D\<^sub>< \<triangleright> C\<^sub>2\<lbrace>q\<rbrace>\<^sub>D"
   by (insert assms, rel_auto) metis+ 
 
-lemma cond_d_hoare_d'_t [hoare_total]: 
+lemma cond_d_hoare_d'_t [hoare_des]: 
   assumes "\<lbrace>p\<rbrace>\<lceil>b\<rceil>\<^sub>D\<^sub>< \<and> C\<^sub>1\<lbrace>q\<rbrace>\<^sub>D" and "\<lbrace>p\<rbrace>\<lceil>\<not>b\<rceil>\<^sub>D\<^sub>< \<and> C\<^sub>2\<lbrace>q\<rbrace>\<^sub>D" 
   shows "\<lbrace>p\<rbrace>bif\<^sub>D b then C\<^sub>1 else C\<^sub>2 eif \<lbrace>q\<rbrace>\<^sub>D"
   by (insert assms, rel_auto) 
@@ -111,50 +111,11 @@ lemma skip_refine_des:
   by pred_auto   
     
 subsection {*Hoare for While-loop*}   
-   
-lemma while_hoare_r_des_t [hoare_total]:
-  assumes WF: "wf R"
-  assumes I0: "`Pre \<Rightarrow> I`"  
-  assumes induct_step:"\<And> st. \<lbrace>b \<and> I \<and> E =\<^sub>u \<guillemotleft>st\<guillemotright>\<rbrace> P \<turnstile> Q \<lbrace>I \<and>(E,\<guillemotleft>st\<guillemotright>)\<^sub>u\<in>\<^sub>u\<guillemotleft>R\<guillemotright>\<rbrace>\<^sub>D"  
-  assumes PHI:"`(\<not>b \<and> I) \<Rightarrow> Post`"  
-  shows "\<lbrace>Pre\<rbrace>while\<^sub>D b invr I do P \<turnstile> Q od\<lbrace>Post\<rbrace>\<^sub>D"
-proof -
-  have M: "mono (\<lambda>X. bif\<^sub>D b then (P \<turnstile> Q) ;; X else SKIP\<^sub>D eif)"
-    by (auto intro: monoI seqr_mono cond_mono) 
-   have H: "(\<lambda>X. bif\<^sub>D b then (P \<turnstile> Q) ;; X else SKIP\<^sub>D eif) \<in> \<lbrakk>\<^bold>H\<rbrakk>\<^sub>H \<rightarrow> \<lbrakk>\<^bold>H\<rbrakk>\<^sub>H" 
-    apply pred_simp apply rel_simp apply smt done   
-  from mono_Monotone_utp_order [OF M, of "\<H>\<^bsub>DES\<^esub>"] H
-          design_theory_continuous.LFP_weak_unfold  
-  have M_des: "Mono\<^bsub>uthy_order DES\<^esub>(\<lambda>X. bif\<^sub>D b then (P \<turnstile> Q) ;; X else SKIP\<^sub>D eif)"
-    by auto
-  show ?thesis    
-  unfolding  hoare_d_def While_inv_des_def While_lfp_des_def
-   apply (rule hoare_pre_str_d_t[unfolded hoare_d_def ,of _ "I" ])
-  using I0 
-   apply pred_simp
-    unfolding ndesign_def rdesign_def
-    apply (rule rec_total_utp_des_rule[where Pre="\<lceil>I\<rceil>\<^sub>D\<^sub><" and E = "E", OF  WF M_des H ])  
-    apply pred_simp
-   apply pred_simp
-  apply (rule  cond_refine_des)
-    subgoal for  st
-      apply (rule_tac seq_refine_unrest_des[ where s= "I \<and> (E,\<guillemotleft>st\<guillemotright>)\<^sub>u\<in>\<^sub>u\<guillemotleft>R\<guillemotright>" ])
-            apply pred_simp
-           apply pred_simp
-       apply (rule order_trans[OF induct_step[unfolded hoare_d_def], where st1 = st]) 
-        apply pred_simp
-        apply pred_simp
-      done
-     apply (rule skip_refine_des)      
-     using PHI
-       apply rel_blast
-  done 
-qed
-  
-lemma while_hoare_r_t [hoare_total]:
+
+lemma while_hoare_r_t [hoare_des]:
   assumes WF: "wf R"
   assumes I0: "`Pre \<Rightarrow> I`"
-  assumes BH :" body is \<^bold>H"  
+  assumes BH :" body is H1"  
   assumes induct_step:"\<And> st. \<lbrace>b \<and> I \<and> E =\<^sub>u \<guillemotleft>st\<guillemotright>\<rbrace> body \<lbrace>I \<and>(E,\<guillemotleft>st\<guillemotright>)\<^sub>u\<in>\<^sub>u\<guillemotleft>R\<guillemotright>\<rbrace>\<^sub>D"  
   assumes PHI:"`(\<not> b \<and> I) \<Rightarrow> Post`"  
   shows "\<lbrace>Pre\<rbrace>while\<^sub>D b invr I do body od\<lbrace>Post\<rbrace>\<^sub>D"
@@ -192,5 +153,44 @@ proof -
   done 
 qed
   
+
+lemma while_hoare_ndesign_t [hoare_des]:
+  assumes WF: "wf R"
+  assumes I0: "`Pre \<Rightarrow> I`"
+  assumes BH :" body is H1"  
+  assumes induct_step:"\<And> st. \<lbrace>b \<and> I \<and> E =\<^sub>u \<guillemotleft>st\<guillemotright>\<rbrace> body \<lbrace>I \<and>(E,\<guillemotleft>st\<guillemotright>)\<^sub>u\<in>\<^sub>u\<guillemotleft>R\<guillemotright>\<rbrace>\<^sub>D"  
+  assumes PHI:"`(\<not> b \<and> I) \<Rightarrow> Post`"  
+  shows "\<lbrace>Pre\<rbrace>while\<^sub>N b invr I do body od\<lbrace>Post\<rbrace>\<^sub>D"
+proof -
+  have M: "mono (\<lambda>X. bif\<^sub>D b then body ;; X else SKIP\<^sub>D eif)"
+    by (auto intro: monoI seqr_mono cond_mono) 
+  have H: "(\<lambda>X. bif\<^sub>D b then body ;; X else SKIP\<^sub>D eif) \<in> \<lbrakk>\<^bold>N\<rbrakk>\<^sub>H \<rightarrow> \<lbrakk>\<^bold>N\<rbrakk>\<^sub>H" 
+    using BH
+    apply pred_simp apply rel_simp  apply smt done   
+  from mono_Monotone_utp_order [OF M, of "\<H>\<^bsub>NDES\<^esub>"] H
+          normal_design_theory_continuous.LFP_weak_unfold  
+  have M_des: "Mono\<^bsub>uthy_order NDES\<^esub>(\<lambda>X. bif\<^sub>D b then body ;; X else SKIP\<^sub>D eif)"
+    by simp
+  show ?thesis    
+  unfolding  hoare_d_def While_inv_ndes_def While_lfp_ndes_def
+   apply (rule hoare_pre_str_d_t[unfolded hoare_d_def ,of _ "I" ])
+  using I0 
+   apply pred_simp
+     apply (rule rec_total_utp_normal_design_rule[where Pre="I" and E = "E", OF WF M_des H])  
+       unfolding ndesign_def rdesign_def
+  apply (rule  cond_refine_des)
+    subgoal for st
+      apply (rule_tac seq_refine_unrest_des[where s= "I \<and> (E,\<guillemotleft>st\<guillemotright>)\<^sub>u\<in>\<^sub>u\<guillemotleft>R\<guillemotright>" ])
+            apply pred_simp
+           apply pred_simp
+       apply (rule order_trans[OF induct_step[unfolded hoare_d_def],  where st1 = st]) 
+        apply pred_simp
+        apply pred_simp
+      done
+     apply (rule skip_refine_des)      
+     using PHI
+       apply rel_blast
+  done 
+qed
 
 end
---
+++ C-verifier/src/Midend-IVL/Isabelle-UTP-Extended/hoare/HoareLogic/TotalCorrectness/IMP_Prog/utp_hoare_ndes_prog.thy
@@ -0,0 +1,108 @@
+theory utp_hoare_ndes_prog
+
+imports "../../../../impl/utp_prog_des_more"
+
+begin
+section {*Hoare logic for designs*}  
+named_theorems hoare_prog
+
+subsection {*Hoare triple definition*}
+
+lift_definition hoare ::   "'\<alpha> cond \<Rightarrow> '\<alpha> prog \<Rightarrow> '\<alpha> cond \<Rightarrow> bool" ("\<lbrace>_\<rbrace>_\<lbrace>_\<rbrace>\<^sub>P") 
+ is  hoare_d .
+
+declare hoare.rep_eq [prog_rep_eq]
+
+lemma hoare_true_assisgns[hoare_prog]: 
+  "\<lbrace>p\<rbrace> \<langle>\<sigma>\<rangle>\<^sub>p \<lbrace>true\<rbrace>\<^sub>P"
+  by (simp add: prog_rep_eq hoare_des)
+
+lemma hoare_true_skip_d_t [hoare_prog]: 
+  "\<lbrace>p\<rbrace>skip\<lbrace>true\<rbrace>\<^sub>P"
+  by (simp add: prog_rep_eq hoare_des)
+
+lemma hoare_false_d_t [hoare_prog]: 
+  "\<lbrace>false\<rbrace>C\<lbrace>q\<rbrace>\<^sub>P"
+  by (simp add: prog_rep_eq hoare_des)
+
+subsection {*Precondition weakening*}
+
+lemma hoare_pre_str_d_t[hoare_prog]:
+  assumes "`p\<^sub>1 \<Rightarrow> p\<^sub>2`" and "\<lbrace>p\<^sub>2\<rbrace>C\<lbrace>q\<rbrace>\<^sub>P"
+  shows "\<lbrace>p\<^sub>1\<rbrace>C\<lbrace>q\<rbrace>\<^sub>P" 
+  using assms  
+  by (simp add: prog_rep_eq hoare_des)
+
+subsection {*Post-condition strengthening*}
+
+lemma hoare_post_weak_d_t[hoare_prog]:
+  assumes "\<lbrace>p\<rbrace>C\<lbrace>q\<^sub>2\<rbrace>\<^sub>P" and "`q\<^sub>2 \<Rightarrow> q\<^sub>1`"
+  shows "\<lbrace>p\<rbrace>C\<lbrace>q\<^sub>1\<rbrace>\<^sub>P" 
+  using assms  
+  by (simp add: prog_rep_eq hoare_des)
+
+subsection {*Hoare and assertion logic*}
+
+lemma hoare_r_conj_d_t [hoare_prog]: 
+  assumes"\<lbrace>p\<rbrace>C\<lbrace>r\<rbrace>\<^sub>P" and "\<lbrace>p\<rbrace>C\<lbrace>s\<rbrace>\<^sub>P"
+  shows "\<lbrace>p\<rbrace>C\<lbrace>r \<and> s\<rbrace>\<^sub>P"
+  using assms  
+  by (simp add: prog_rep_eq hoare_des)
+
+subsection {*Hoare SKIP*}
+
+lemma skip_d_hoare_d_t [hoare_prog]: 
+  "\<lbrace>p\<rbrace>skip\<lbrace>p\<rbrace>\<^sub>P"
+  by (simp add: prog_rep_eq hoare_des)
+
+subsection {*Hoare for assignment*}
+
+lemma assigns_d_hoare_d_t [hoare_prog]: 
+  assumes"`p \<Rightarrow> \<sigma> \<dagger> q`" 
+  shows  "\<lbrace>p\<rbrace>\<langle>\<sigma>\<rangle>\<^sub>p\<lbrace>q\<rbrace>\<^sub>P"
+  using assms  
+  by (simp add: prog_rep_eq hoare_des)
+
+lemma assigns_d_hoare_d'_t [hoare_prog]: 
+  "\<lbrace>\<sigma> \<dagger> p\<rbrace>\<langle>\<sigma>\<rangle>\<^sub>p\<lbrace>p\<rbrace>\<^sub>P"
+  by (simp add: prog_rep_eq hoare_des)
+
+lemma assigns_d_floyd_d_t [hoare_prog]:
+  assumes \<open>vwb_lens x\<close>
+  shows \<open>\<lbrace>p\<rbrace>x :== e\<lbrace>\<^bold>\<exists>v \<bullet> p\<lbrakk>\<guillemotleft>v\<guillemotright>/x\<rbrakk> \<and> &x =\<^sub>u e\<lbrakk>\<guillemotleft>v\<guillemotright>/x\<rbrakk>\<rbrace>\<^sub>P\<close>
+  using assms
+  by (simp add: prog_rep_eq hoare_des)
+
+subsection {*Hoare for Sequential Composition*}
+
+lemma seq_hoare_d_t [hoare_prog]: 
+  assumes"\<lbrace>p\<rbrace>C\<^sub>1\<lbrace>s\<rbrace>\<^sub>P" and "\<lbrace>s\<rbrace>C\<^sub>2\<lbrace>r\<rbrace>\<^sub>P" 
+  shows"\<lbrace>p\<rbrace>C\<^sub>1 ; C\<^sub>2\<lbrace>r\<rbrace>\<^sub>P"
+  using assms
+  by (simp add: prog_rep_eq hoare_des)  
+
+subsection {*Hoare for Conditional*}
+
+lemma cond_d_hoare_d_t [hoare_prog]: 
+  assumes "\<lbrace>b \<and> p\<rbrace>C\<^sub>1\<lbrace>q\<rbrace>\<^sub>P" and "\<lbrace>\<not>b \<and> p\<rbrace>C\<^sub>2\<lbrace>q\<rbrace>\<^sub>P" 
+  shows "\<lbrace>p\<rbrace>bif b then C\<^sub>1 else C\<^sub>2 eif\<lbrace>q\<rbrace>\<^sub>P"
+  using assms
+  by (simp add: prog_rep_eq hoare_des) 
+    
+subsection {*Hoare for While-loop*}   
+
+lemma normal_design_is_H1:
+  "P is \<^bold>N \<Longrightarrow> P is H1"
+  by (metis  H3_ndesign Healthy_def' ndesign_form)     
+
+lemma while_hoare_r_t [hoare_prog]:
+  assumes WF: "wf R"
+  assumes I0: "`Pre \<Rightarrow> I`" 
+  assumes induct_step:"\<And> st. \<lbrace>b \<and> I \<and> E =\<^sub>u \<guillemotleft>st\<guillemotright>\<rbrace> body \<lbrace>I \<and>(E,\<guillemotleft>st\<guillemotright>)\<^sub>u\<in>\<^sub>u\<guillemotleft>R\<guillemotright>\<rbrace>\<^sub>P"  
+  assumes PHI:"`(\<not> b \<and> I) \<Rightarrow> Post`"  
+  shows "\<lbrace>Pre\<rbrace>pwhile_inv_des b  I  body \<lbrace>Post\<rbrace>\<^sub>P"
+  by (simp add:  prog_rep_eq  
+                 while_hoare_ndesign_t[OF WF I0 Rep_prog_H1_H3_closed [of body, THEN normal_design_is_H1]
+                                        induct_step [unfolded prog_rep_eq] PHI])
+
+end
