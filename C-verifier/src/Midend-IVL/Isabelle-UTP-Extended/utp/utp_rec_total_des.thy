ea674610d42b38bc363c9fda073701280b8767a1
Yakoub Nemouchi <YakoubN@users.noreply.github.com>
2017-10-23 16:12:53 +0000

    minor comments
    git-svn-id: orca@505

--- C-verifier/src/Midend-IVL/Isabelle-UTP-Extended/utp/utp_rec_total_des.thy
+++ C-verifier/src/Midend-IVL/Isabelle-UTP-Extended/utp/utp_rec_total_des.thy
@@ -2,12 +2,12 @@ theory utp_rec_total_des
   imports "../hoare/AlgebraicLaws/Design/Algebraic_laws_design"
 begin
 
-lemma mu_design_is_healthy_des[simp]:
+lemma mu_design_is_healthy_des[simp]: (*This should be genrated automatically in utp_theory*)
   "\<mu>\<^sub>D B is \<H>\<^bsub>DES\<^esub>"    
   by (metis (no_types) Healthy_def' des_hcond_def design_theory_continuous.LFP_closed)
 
 
-lemma mu_normal_design_is_healthy_des[simp]:
+lemma mu_normal_design_is_healthy_des[simp]: (*This should be genrated automatically in utp_theory*)
   "\<mu>\<^sub>N B is \<H>\<^bsub>NDES\<^esub>"
   by (metis (no_types) Healthy_def' ndes_hcond_def normal_design_theory_continuous.LFP_closed) 
 
@@ -16,17 +16,17 @@ lemma design_is_healthy_des:"$ok\<acute> \<sharp> Pre \<Longrightarrow> Pre \<tu
   apply fastforce
 done    
 
-lemma rdesign_is_healthy_des[simp]:
+lemma rdesign_is_healthy_des[simp]: (*This should be genrated automatically in utp_theory*)
   "Pre \<turnstile>\<^sub>r Post is \<H>\<^bsub>DES\<^esub>"
   by (simp add: rdesign_def design_is_healthy_des unrest)  
 
-lemma ndesign_is_healthy_des[simp]:
+lemma ndesign_is_healthy_des[simp]: (*This should be genrated automatically in utp_theory*)
   "Pre \<turnstile>\<^sub>n Post is \<H>\<^bsub>NDES\<^esub>"
   apply rel_simp 
   apply fastforce
 done
     
-lemma Mono_utp_orderD:
+lemma Mono_utp_orderD: (*This should be genrated automatically in utp_theory*)
   assumes M: "Mono\<^bsub>utp_order H\<^esub> B"
   and     "x is H"
   and     "y is H"
@@ -73,7 +73,6 @@ proof -
     by pred_simp
 qed    
   
-
 lemma rdesign_mu_wf_refine_intro: 
   assumes   WF: "wf R"
     and      M: "Mono\<^bsub>uthy_order DES\<^esub> B"
